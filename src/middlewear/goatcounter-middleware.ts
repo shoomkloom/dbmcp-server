3021943ff5957c1f487ad07fe3c458778a156f1a
shoomkloom <shoomkloom@gmail.com>
2025-09-10 18:06:01 +0300

    Added logging

--- src/middlewear/goatcounter-middleware.ts
+++ src/middlewear/goatcounter-middleware.ts
@@ -7,28 +7,29 @@ type GoatCounterOpts = {
 };
 
 export function goatCounter(opts: GoatCounterOpts = {}) {
-  const site = opts.site ?? process.env.GOATCOUNTER_SITE;
-  const token = opts.token ?? process.env.GOATCOUNTER_TOKEN;
-  const noSessions = opts.noSessions ?? true;
+    console.log("GoatCounter middleware Invoked");
+    const site = opts.site ?? process.env.GOATCOUNTER_SITE;
+    const token = opts.token ?? process.env.GOATCOUNTER_TOKEN;
+    const noSessions = opts.noSessions ?? true;
 
-  const disabled = !site || !token;
+    const disabled = !site || !token;
 
-  return function goatCounterMiddleware(req: any, _res: any, next: any) {
-    if (!disabled) {
-      const path = opts.fixedPath || req.path || "/";
+    return function goatCounterMiddleware(req: any, _res: any, next: any) {
+        if (!disabled) {
+        const path = opts.fixedPath || req.path || "/";
 
-      fetch(`https://${site}/api/v0/count`, {
-        method: "POST",
-        headers: {
-          "content-type": "application/json",
-          authorization: `Bearer ${token}`,
-        },
-        body: JSON.stringify({
-          no_sessions: noSessions,
-          hits: [{ path }],
-        }),
-      }).catch(() => { console.warn("GoatCounter logging failed"); });
-    }
-    next();
-  };
+        fetch(`https://${site}/api/v0/count`, {
+            method: "POST",
+            headers: {
+            "content-type": "application/json",
+            authorization: `Bearer ${token}`,
+            },
+            body: JSON.stringify({
+            no_sessions: noSessions,
+            hits: [{ path }],
+            }),
+        }).catch(() => { console.warn("GoatCounter logging failed"); });
+        }
+        next();
+    };
 }
